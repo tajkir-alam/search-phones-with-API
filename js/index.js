b1e47ae5db71934f1496fb3812129ac03c57fc00
tajkir-alam <tajkir.alam@gmail.com>
2023-02-26 20:07:23 +0600

    cards added dynamically

--- js/index.js
+++ js/index.js
@@ -1,14 +1,35 @@
-const loadPhones = async () => {
-    const URL = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async (search) => {
+    const URL = `https://openapi.programming-hero.com/api/phones?search=${search}`;
     const res = await fetch(URL);
     const data = await res.json();
-    displayPhones(data);
+    displayPhones(data.data);
 }
 
 const displayPhones = phones => {
-    console.log(phones);
+    const cardContainer = document.getElementById('card-container');
+    phones.forEach(phone => {
+        console.log(phone)
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML = `
+        <div class="card h-100 border-0 shadow">
+            <div class="text-center py-3">
+                <img src="${phone.image}" class="card-img-top w-50" alt="...">
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button class="btn btn-primary">Details</button>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated 3 mins ago</small>
+            </div>
+        </div>
+        `   
+        cardContainer.appendChild(cardDiv);
+    });
 }
 
 
 
-loadPhones()
+loadPhones('iphone')
